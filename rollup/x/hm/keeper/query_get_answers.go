28da1446a838fb61ab1a6453caf8e679465da85b
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2024-02-20 15:45:44 +0100

    scaffold get-answers

--- rollup/x/hm/keeper/query_get_answers.go
+++ rollup/x/hm/keeper/query_get_answers.go
@@ -3,10 +3,14 @@ package keeper
 import (
 	"context"
 
+	"hm/x/hm/types"
+
+	"cosmossdk.io/store/prefix"
+	"github.com/cosmos/cosmos-sdk/runtime"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"hm/x/hm/types"
 )
 
 func (k Keeper) GetAnswers(goCtx context.Context, req *types.QueryGetAnswersRequest) (*types.QueryGetAnswersResponse, error) {
@@ -17,7 +21,29 @@ func (k Keeper) GetAnswers(goCtx context.Context, req *types.QueryGetAnswersRequ
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Process the query
+
 	_ = ctx
 
-	return &types.QueryGetAnswersResponse{}, nil
+	var answers []types.Answer
+
+	store := runtime.KVStoreAdapter(k.storeService.OpenKVStore(ctx))
+	answerStore := prefix.NewStore(store, types.KeyPrefix(types.AnswerKeyPrefix))
+
+	pageRes, err := query.Paginate(answerStore, req.Pagination, func(key []byte, value []byte) error {
+		var answer types.Answer
+		if err := k.cdc.Unmarshal(value, &answer); err != nil {
+			return err
+		}
+
+		if req.Qid == answer.Index {
+			answers = append(answers, answer)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryGetAnswersResponse{Answers: answers, Pagination: pageRes}, nil
 }
