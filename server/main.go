1296addceed4aaa244704857c08981c41e0a1ee0
Tomasz Zdybał <tomek@zdybal.lap.pl>
2024-02-20 15:57:08 +0100

    main program

--- server/main.go
+++ server/main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net"
-
+	"context"
+	"flag"
 	"github.com/Manav-Aggarwal/herd-mentality/server/pb/hm"
 	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 12345))
+	questionsPtr := flag.String("questions", "", "Path to file with questions")
+	laddrPtr := flag.String("laddr", "0.0.0.0:9988", "Listen address (tcp/ip)")
+
+	flag.Parse()
+
+	log.Println("questions file:", *questionsPtr)
+	log.Println("laddr:", *laddrPtr)
+
+	lis, err := net.Listen("tcp", *laddrPtr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	hm.RegisterHerdMentalityServer(grpcServer, &server{})
+
+	srv := &server{}
+	file, err := os.Open(*questionsPtr)
+	if err != nil {
+		log.Fatalf("cannot load questions file: %v", err)
+	}
+	srv.loadQuestions(file)
+	log.Printf("# of loaded questions: %d", len(srv.questions))
+
+	srv.start(context.Background())
+	hm.RegisterHerdMentalityServer(grpcServer, srv)
 
 	grpcServer.Serve(lis)
-
 }
