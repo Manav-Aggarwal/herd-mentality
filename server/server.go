4aa57d2a4fc5c4e54c525bf3fe06dc87dbc9b316
Tomasz Zdybał <tomek@zdybal.lap.pl>
2024-02-20 15:32:49 +0100

    test: simple TestLoadQuestions

--- server/server.go
+++ server/server.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
-
 	"github.com/Manav-Aggarwal/herd-mentality/server/pb/hm"
+	"io"
 )
 
 type server struct {
+	questions []string
 }
 
 func (s *server) CurrentQuestion(ctx context.Context, request *hm.QuestionRequest) (*hm.Question, error) {
@@ -22,4 +23,7 @@ func (s *server) SubmitAnswer(ctx context.Context, answer *hm.Answer) (*hm.Confi
 func (s *server) GetResults(ctx context.Context, request *hm.ResultsRequest) (*hm.Results, error) {
 	//TODO implement me
 	panic("implement me")
+}
+
+func (s *server) loadQuestions(questions io.Reader) {
 }
--- server/server_test.go
+++ server/server_test.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadQuestions(t *testing.T) {
+	s := &server{}
+
+	questions := strings.NewReader("question 1\nquestion 2\n question3\n")
+
+	s.loadQuestions(questions)
+	assert.Len(t, s.questions, 3)
+}
